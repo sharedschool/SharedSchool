67063e6355a8a7715d557d42f619171f00a270c9
Me 268462 <268462@sharedschool.nfshost.com>
2019-06-22 20:41:51 +0000

    Updated email sender

--- handlers/demo.php
+++ handlers/demo.php
@@ -1,13 +1,14 @@
 <?php
 	$to = 'contact@sharedschool.co';
-	$from = 'no-reply@sharedschool.co';
+	$from = 'SharedSchool No Reply <no-reply@sharedschool.co>';
 	$subject = 'Demo Request';
 	
-	$msg = 'Full Name: ' . $_POST['name'] . '\nInstitution: ' . $_POST['institution'] . '\nEmail: ' . $_POST['email']
+	$msg = 'Full Name: ' . $_POST['name'] . '\nInstitution: ' . 
+$_POST['institution'] . '\nEmail: ' . $_POST['email'];
 	
 	$msg = wordwrap($msg,70);
 	$headers = 'From: ' . $from;
-	mail($to,$subject,$txt,$headers);
+	mail($to,$subject,$msg,$headers);
 	
-	header('Location: /?demo=1')
+	header('Location: /?demo=1');
 ?>
