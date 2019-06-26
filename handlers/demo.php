c331fc0eed1264ca2e277bbeb552ea50b9eb1f9e
peterapps <peterapps2016@gmail.com>
2019-06-26 12:17:16 -0400

    Demo request uses HTML email

--- handlers/demo.php
+++ handlers/demo.php
@@ -9,9 +9,12 @@ $_POST['institution'] . '\nEmail: ' . $_POST['email'];
 	if (isset($_POST['message']) && strlen($_POST['message']) > 0){
 		$msg .= '\n\nMessage: ' . $_POST['message'];
 	}
-	
 	$msg = wordwrap($msg,70);
+	$msg = str_replace('\n', '<br/>', $msg);
+	$msg = '<html><body>' . $msg . '</body></html>';
 	$headers = 'From: ' . $from;
+	$headers .= 'MIME-Version: 1.0' . '\r\n';
+	$headers .= 'Content-type:text/html;charset=UTF-8' . '\r\n';
 	mail($to,$subject,$msg,$headers);
 	
 	header('Location: /?demo=1');
