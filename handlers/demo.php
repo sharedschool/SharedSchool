36428a7321be9b01f5e1750a8fabb8d41e8cdd73
Me 268462 <268462@sharedschool.nfshost.com>
2019-06-26 16:47:07 +0000

    Simpler email

--- handlers/demo.php
+++ handlers/demo.php
@@ -3,18 +3,13 @@
 	$from = 'SharedSchool No Reply <no-reply@sharedschool.co>';
 	$subject = 'Demo Request';
 	
-	$msg = 'Full Name: ' . $_POST['name'] . '\nInstitution: ' . 
-$_POST['institution'] . '\nEmail: ' . $_POST['email'];
+	$msg = 'Full Name: ' . $_POST['name'] . '. Institution: ' . 
+$_POST['institution'] . '. Email: ' . $_POST['email'];
 
 	if (isset($_POST['message']) && strlen($_POST['message']) > 0){
-		$msg .= '\n\nMessage: ' . $_POST['message'];
+		$msg .= '. Message: ' . $_POST['message'];
 	}
-	$msg = wordwrap($msg,70);
-	//$msg = str_replace('\n', '<br/>', $msg);
-	//$msg = '<html><body>' . $msg . '</body></html>';
-	$headers = 'From: ' . $from;
-	$headers .= 'MIME-Version: 1.0' . '\r\n';
-	$headers .= 'Content-type:text/plain;charset=UTF-8' . '\r\n';
+	$headers = 'From: ' . $from . '\r\n';
 	mail($to,$subject,$msg,$headers);
 	
 	header('Location: /?demo=1');
