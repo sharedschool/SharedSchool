de0b4e4b9be39be14b177d62a1371a610c1e78c4
peterapps <peterapps2016@gmail.com>
2019-07-02 21:10:34 -0400

    Added check so only HTTPS when hosted

--- helper/launcher.php
+++ helper/launcher.php
@@ -1,13 +1,15 @@
 <?php
 	session_start();
-	if (!(isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == 'on' || 
-		$_SERVER['HTTPS'] == 1) ||  
-		isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&   
-		$_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https'))
-	{
-		$redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-		header('HTTP/1.1 301 Moved Permanently');
-		header('Location: ' . $redirect);
-		exit();
+	if (strpos( $_SERVER['HTTP_HOST'], 'sharedschool.co') !== false){
+		if (!(isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == 'on' || 
+			$_SERVER['HTTPS'] == 1) ||  
+			isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&   
+			$_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https'))
+		{
+			$redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+			header('HTTP/1.1 301 Moved Permanently');
+			header('Location: ' . $redirect);
+			exit();
+		}
 	}
 ?>
